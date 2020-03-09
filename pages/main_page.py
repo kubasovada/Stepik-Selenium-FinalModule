b73c30dba0dda9e1b37d19c6cdefd0bce92f70e9
Darya <kubasovada@mail.ru>
2020-03-09 18:10:12 +0300

    Добавлен метод test_guest_can_go_to_login_page, инициализируем LoginPage в теле теста

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,20 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
+from pages.login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
