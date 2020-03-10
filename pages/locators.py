70193c46e8a5132ae0bf79adb5b340314380f1a0
Darya <kubasovada@mail.ru>
2020-03-10 16:28:43 +0300

    class ProductPageLocators modified

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,13 @@ class MainPageLocators:
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form_invalid")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_page  h1")
+    ADD_MESSAGE = (By.CSS_SELECTOR, "#messages .alertinner")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    CART_PRICE = (By.CSS_SELECTOR, "#messages .alertinner > p:first-child strong")
 
