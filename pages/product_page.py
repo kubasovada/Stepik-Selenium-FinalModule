52e4671b69b91783677b824c0af14d520755d17d
Darya <kubasovada@mail.ru>
2020-03-10 16:32:10 +0300

    class ProductPage added

---
+++ pages/product_page.py
@@ -0,0 +1,32 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def should_be_add_to_basket_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Add_to_basket button is not presented"
+
+    def add_to_cart(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+
+    def give_product_name(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        return product_name
+
+    def give_product_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        return product_price
+
+    def give_basket_price(self):
+        cart_price = self.browser.find_element(*ProductPageLocators.CART_PRICE).text
+        return cart_price
+
+    def should_be_success_message(self):
+        self.product_name = self.give_product_name()
+        message = self.browser.find_element(*ProductPageLocators.ADD_MESSAGE).text
+        assert  self.product_name in message, "Product is not added to basket"
+
+    def should_be_cart_value(self):
+        assert self.give_basket_price() == self.give_product_price(), "The cost of the basket doesn't match the price of the product"
+
+
