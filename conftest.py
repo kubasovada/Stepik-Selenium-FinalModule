7da7539335ca7a99003280fe48592113859a0cea
Darya <kubasovada@mail.ru>
2020-03-17 16:06:07 +0300

    Choosing --browser added

--- conftest.py
+++ conftest.py
@@ -6,13 +6,23 @@ from datetime import datetime
 
 def pytest_addoption(parser):
     parser.addoption('--language', action = 'store', default = 'ru', help = 'Choose language: ')
+    parser.addoption('--browser', action = 'store', default = 'Chrome', help = 'Choose browser')
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome()
+    browser_name = request.config.getoption("browser")
+    browser = None
+    if browser_name == "Chrome":
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "Firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser name should be Chrome or Firefox")
     browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
